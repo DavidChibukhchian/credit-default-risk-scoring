b70e0968c294c51dcbe22551d9aed012e3185d7e
David <d.chibukhchian@gmail.com>
2026-02-19 22:24:50 +0300

    pre-commit + package skeleton

---
+++ credit_scoring/infer.py
@@ -0,0 +1,6 @@
+def main() -> None:
+    print("infer: TODO")
+
+
+if __name__ == "__main__":
+    main()
---
+++ credit_scoring/train.py
@@ -0,0 +1,6 @@
+def main() -> None:
+    print("train: TODO")
+
+
+if __name__ == "__main__":
+    main()
